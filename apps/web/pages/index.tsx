19249f1405595a09447e7ee6fed6993fb630c453
Gw3i <contact.johngoldco@gmail.com>
2023-03-19 14:03:28 +0100

    Delete env-file

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,11 @@
+import { faMobileAlt } from "@fortawesome/free-solid-svg-icons";
 import { Button, Input } from "ui/components";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
-      <Button size="large" text="Buttontext" />
+      <Button size="large" text="Buttontext" icon={faMobileAlt} />
       <Input type={"search"} />
     </div>
   );
