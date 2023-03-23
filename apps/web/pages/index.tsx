1b8040510acfe5018e6e4072dcbcffe21f3fbe79
Swappnet <advance11ua@gmail.com>
2023-03-23 12:16:43 +0200

    optimized font loading

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,25 +1,23 @@
-import dynamic from "next/dynamic"
-import { useAuth } from "../context/AuthContext"
-import { Header, Input } from "ui"
-import Head from "next/head"
+import dynamic from 'next/dynamic';
+import { useAuth } from '../context/AuthContext';
+import { Header, Input } from 'ui';
+import Head from 'next/head';
 
 export default function Web() {
-    const { user } = useAuth()
+  const { user } = useAuth();
 
-    const DynamicLayout = dynamic(() => import("../components/Layout"), {
-        ssr: false,
-    })
+  const DynamicLayout = dynamic(() => import('../components/Layout'), {
+    ssr: false,
+  });
 
-    return (
-        <DynamicLayout>
-            <Head>
-                <title>Home</title>
-            </Head>
-            {/* Put here already styled Home page component & remove Header! */}
-            <Header
-                variant="welcome"
-                userName={user ? user?.username : "Guest"}
-            />
-        </DynamicLayout>
-    )
+  return (
+    <DynamicLayout>
+      <Head>
+        <title>Home</title>
+      </Head>
+      {/* Put here already styled Home page component & remove Header! */}
+      <Header variant="welcome" userName={user ? user?.username : 'Guest'} />
+      <Input variant="search" />
+    </DynamicLayout>
+  );
 }
--- packages/ui/components/global-styles/GlobalStyle.tsx
+++ packages/ui/components/global-styles/GlobalStyle.tsx
@@ -2,8 +2,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600&family=Poppins:wght@300;400;500;600;700;800&display=swap');
   :root {
     
     --btn-primary: #1F986C;
